3e96f826df855d6628c7b108c189f552e7149d09
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-20 12:47:56 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,7 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready..."
 sleep 30s;
 
+docker-compose exec -T mosquitto mosquitto_passwd -U /mosquitto/data/mosquitto.password_file
+
+docker-compose down;
+docker-compose up -d;
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,7 +1,14 @@
 #set env vars
-#set -o allexport; source .env; set +o allexport;
+set -o allexport; source .env; set +o allexport;
 
 
 
 mkdir ./mosquitto
 chown -R 1000:1000 ./mosquitto
+
+cat << EOT >> ./mosquitto/data/mosquitto.password_file
+
+admin:${ADMIN_PASSWORD}
+EOT
+
+chown 0700 ./mosquitto/data/mosquitto.password_file
