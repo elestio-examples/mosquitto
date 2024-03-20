ff806339d2371a6401374cda32e31ece2d848e6c
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-20 13:03:19 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -7,8 +7,7 @@ mkdir -p ./mosquitto/{config,data,log}
 chown -R 1000:1000 ./mosquitto
 
 cat << EOT >> ./mosquitto/data/mosquitto.password_file
-
 admin:${ADMIN_PASSWORD}
 EOT
 
-chown 0700 ./mosquitto/data/mosquitto.password_file
+chmod 0700 ./mosquitto/data/mosquitto.password_file
